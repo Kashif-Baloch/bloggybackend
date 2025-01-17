b4501b4710fc717c22f508d8acf4ce27b73903dd
Kashif Nawaz <kashifnawaz@gmail.com>
2025-01-17 18:26:10 +0500

    done some changes in db file

--- utils/db.js
+++ utils/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const connectToMongo = async () => {
   try {
+    console.log(process.env.MONGO_URI_PRODUCTION);
     await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
     console.log("Connected To MongoDB Successfully");
   } catch (error) {
+    console.log(process.env.MONGO_URI_PRODUCTION);
     console.log(error.message);
   }
 };
